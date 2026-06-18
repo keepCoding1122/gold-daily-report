c4f411ee6e598fc9e5e9ab16ade0b4e2af81cf54
xiech <xiech@hello-tech.com>
2026-06-18 17:17:48 +0800

    chore: remove temp commit script

--- _clean.bat
+++ _clean.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d D:\gold-daily-report
 git add --all
-git commit -m "fix: KeyError crash + add AI summary + optimize layout"
+git commit -m "chore: remove temp commit script"
 git push origin master
